d68a1d40a6a4539470ec4e3c883be9b1f89e248f
James Byrne <jamesdesbyrne@gmail.com>
2017-06-08 20:52:49 +0100

    Updating solution

--- lib/Solution.ex
+++ lib/Solution.ex
@@ -2,19 +2,22 @@ defmodule Kata do
 
   def start(string) do
     [head|tail] = String.codepoints(string)
-    compress_string(head, tail, 0, "")
+    compress(head, tail, 0, "")
   end
 
-  def compress_string(head, [head|tail], counter, list) do
-    compress_string(head, tail, counter + 1, list)
+  def compress(letter, [], counter, list), do: list <> to_string(counter + 1) <> letter
+  def compress(head, [head|tail], counter, list), do: compress(head, tail, counter + 1, list)
+  def compress(letter, [head|tail], counter, list) do
+    compress(head, tail, 0, list <> to_string(counter + 1) <> letter)
   end
 
-  def compress_string(letter, [head|tail], counter, list) do
-    compress_string(head, tail, 0, list <> to_string(counter + 1) <> letter)
+  def decode(string), do: IO.puts decode(String.codepoints(string), "")
+  def decode([], list), do: list
+  def decode([number, letter | tail], list) do
+    decode(tail, append(String.to_integer(number), letter, list))
   end
 
-  def compress_string(letter, [], counter, list) do
-    list <> to_string(counter + 1) <> letter
-  end
+  def append(0, _, list), do: list
+  def append(number, letter, list), do: append(number - 1, letter, list <> letter)
 
 end
